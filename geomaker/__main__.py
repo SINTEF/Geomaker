9c80989b2e5e2a0a09f89a0c2bbf12677bb6ec02
Eivind Fonn <evfonn@gmail.com>
2020-01-22 14:38:14 +0100

    Info box with bounds

--- geomaker/__main__.py
+++ geomaker/__main__.py
@@ -9,7 +9,7 @@ from PyQt5.QtCore import (
 from PyQt5.QtWebChannel import QWebChannel
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtWidgets import (
-    QApplication, QWidget, QMainWindow, QVBoxLayout, QHBoxLayout, QListView, QLabel, QInputDialog, QSplitter
+    QApplication, QWidget, QMainWindow, QVBoxLayout, QGridLayout, QListView, QLabel, QInputDialog, QSplitter
 )
 
 from geomaker.db import Database, Polygon
@@ -19,6 +19,15 @@ def label(text):
     label = QLabel()
     label.setText(text)
     return label
+
+
+def angle_to_degrees(angle, directions):
+    direction = directions[1 if angle > 0 else 0]
+    angle = abs(angle)
+    degrees, angle = divmod(angle, 1.0)
+    minutes, angle = divmod(60 * angle, 1.0)
+    seconds = 60 * angle
+    return f"{degrees:.0f}° {minutes:.0f}' {seconds:.3f}'' {direction}"
 
 
 class DatabaseModel(QAbstractListModel):
@@ -81,17 +90,59 @@ class JSInterface(QObject):
             self.db_widget.select(self.db.index_of(lfid=lfid))
 
 
-class DatabaseWidget(QWidget):
+class PolyWidget(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.setVisible(False)
+        self.create_ui()
+
+    def _add_row(self, attrname, title, widget):
+        self._rows += 1
+        self.box.addWidget(label(title), self._rows, 0, Qt.AlignRight)
+        self.box.addWidget(widget, self._rows, 1, Qt.AlignLeft)
+        setattr(self, attrname, widget)
+
+    def create_ui(self):
+        self.box = QGridLayout()
+        self.setLayout(self.box)
+        self._rows = 0
+
+        self.name = label('')
+        self.box.addWidget(self.name, 0, 0, 1, 2, Qt.AlignCenter)
+
+        self._add_row('west', 'West', label(''))
+        self._add_row('east', 'East', label(''))
+        self._add_row('south', 'South', label(''))
+        self._add_row('north', 'North', label(''))
+
+        self.box.setRowStretch(self._rows + 1, 1)
+
+    def show(self, poly=None):
+        if poly is None:
+            self.setVisible(False)
+            return
+        self.setVisible(True)
+        self.name.setText(f'<strong>{poly.name}</strong>')
+        self.west.setText(f'{poly.west:.4f} ({angle_to_degrees(poly.west, "WE")})')
+        self.east.setText(f'{poly.east:.4f} ({angle_to_degrees(poly.east, "WE")})')
+        self.south.setText(f'{poly.south:.4f} ({angle_to_degrees(poly.south, "SN")})')
+        self.north.setText(f'{poly.north:.4f} ({angle_to_degrees(poly.north, "SN")})')
+
+
+class DatabaseWidget(QSplitter):
 
     def __init__(self, main):
         super().__init__()
+        self.setOrientation(Qt.Vertical)
         self.main = main
         self.db = main.db
         self.create_ui()
 
     def create_ui(self):
+        top = QWidget()
         box = QVBoxLayout()
-        self.setLayout(box)
+        top.setLayout(box)
 
         box.addWidget(label('<strong>Stored regions</strong>'))
 
@@ -100,6 +151,11 @@ class DatabaseWidget(QWidget):
         self.listview.selectionModel().selectionChanged.connect(self.selection_changed)
         self.listview.doubleClicked.connect(self.list_double_clicked)
         box.addWidget(self.listview)
+
+        self.addWidget(top)
+
+        self.poly = PolyWidget()
+        self.addWidget(self.poly)
 
     def unselect(self):
         selection_model = self.listview.selectionModel()
@@ -115,8 +171,11 @@ class DatabaseWidget(QWidget):
             index = selected.indexes()[0]
         except IndexError:
             self.main.set_selected()
+            self.poly.show()
             return
-        self.main.set_selected(self.db[index.row()].lfid)
+        poly = self.db[index.row()]
+        self.main.set_selected(poly.lfid)
+        self.poly.show(poly)
 
     def list_double_clicked(self, item):
         self.main.focus(self.db[item.row()].lfid)
