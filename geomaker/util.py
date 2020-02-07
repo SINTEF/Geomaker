4285880feab898d7caa9eb121e09a4466ae94fb8
Eivind Fonn <evfonn@gmail.com>
2020-02-07 13:49:10 +0100

    Move filesystem-related code to its own module

---
+++ geomaker/util.py
@@ -0,0 +1,9 @@
+class SingletonMeta(type):
+    """Metaclass for creating singleton classes."""
+
+    __instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls.__instances:
+            cls.__instances[cls] = super().__call__(*args, **kwargs)
+        return cls.__instances[cls]
